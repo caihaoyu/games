11a711eda043088101bcc2727052e5b8148978c7
caihaoyu <chy@caihaoyutekiMacBook-Pro.local>
2013-10-23 15:05:18 +0800

    删除修改不需要的文件，增加roundserviceimpl的序列化

--- src/main/java/com/chy/service/impl/RoundServiceImpl.java
+++ src/main/java/com/chy/service/impl/RoundServiceImpl.java
@@ -1,43 +1,46 @@
 package com.chy.service.impl;
 
-import com.chy.dao.RoundDao;
-import com.chy.model.Round;
-import com.chy.service.RoundService;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.chy.dao.RoundDao;
+import com.chy.model.Round;
+import com.chy.service.RoundService;
+
 @Service("roundService")
-@Transactional(readOnly=true)
-public class RoundServiceImpl
-  implements RoundService
-{
+@Transactional(readOnly = true)
+public class RoundServiceImpl extends AbstractServiceImpl<Round, String>
+		implements RoundService {
 
-  @Autowired
-  RoundDao dao;
+	@Autowired
+	public RoundServiceImpl(RoundDao dao) {
+		super(dao);
+		this.dao=dao;
+		// TODO Auto-generated constructor stub
+	}
 
-  @Transactional(readOnly=true)
-  public Round getRound(String id)
-  {
-    return ((Round)this.dao.findById(id));
-  }
+	RoundDao dao;
 
-  @Transactional(readOnly=false)
-  public Round saveOrUpdate(Round round)
-  {
-    return this.dao.saveOrUpdate(round);
-  }
+	@Transactional(readOnly = true)
+	public Round getRound(String id) {
+		return ((Round) this.dao.findById(id));
+	}
 
-  @Transactional(readOnly=false)
-  public void deleteRound(Round round)
-  {
-    this.dao.delete(round);
-  }
+	@Transactional(readOnly = false)
+	public Round saveOrUpdate(Round round) {
+		return this.dao.saveOrUpdate(round);
+	}
 
-  @Transactional(readOnly=true)
-  public List<Round> findAllRound()
-  {
-    return this.dao.findAll();
-  }
+	@Transactional(readOnly = false)
+	public void deleteRound(Round round) {
+		this.dao.delete(round);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Round> findAllRound() {
+		return this.dao.findAll();
+	}
 }
